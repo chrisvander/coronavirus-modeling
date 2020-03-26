978dbd7e5168a0daed40ea170aeb0761ec451e2b
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-03-26 14:23:21 -0400

    urban actor

--- population.py
+++ population.py
@@ -1,3 +1,16 @@
+class Person:
+  def __init__(self, age, income, gender):
+    self.age = age
+    self.income = income
+    self.gender = gender
+    self.household = 0
+
+class UrbanActor:
+  def __init__(self, person):
+    self.demographic = {}
+    self.demographic.update(person.__dict__)
+
 def generate(n, config):
   print("-- GENERATE POPULATION --")
-  print(config)
+  p = Person(23, 50000, 'male')
+  ua = UrbanActor(p)
