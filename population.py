c73ea2e8db7d9875cbfbaa9327245309ddcdd5cb
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-04-06 18:21:16 -0400

    leaves some orphan children behind but thats ok

--- population.py
+++ population.py
@@ -1,5 +1,7 @@
 from util.webapi import get_json, parse_query
 import numpy as np
+from scipy.stats import norm
+import itertools
 import random
 
 base_url = "https://api.census.gov/data/2018/acs/acs1/pums?get="
@@ -7,7 +9,7 @@ base_url = "https://api.census.gov/data/2018/acs/acs1/pums?get="
 def_region = "7950000US3703001,7950000US3703002"
 
 def get_household_json(region):
-  url = base_url + 'PWGTP,AGEP,HHT,HUPAC,NOC'
+  url = base_url + 'PWGTP,AGEP,HHT,NP'
   if region is not None:
     url = url + '&ucgid=' + region
   return get_json(url, {})
@@ -24,7 +26,25 @@ def get_person_json(region):
 
 class Household:
   def __init__(self):
-    pass
+    self.people = []
+    self.head = None
+    self.spouse = None
+    self.otherMembers = []
+  def addPerson(self,person):
+    self.people.append(person)
+    self.otherMembers.append(person)
+  def getPeople(self):
+    return self.people
+  def getOtherMembers(self):
+    return self.otherMembers
+  def addHead(self, person):
+    self.people.append(person)
+    self.head = person
+  def addSpouse(self, person):
+    self.people.append(person)
+    self.spouse = person
+  def getSize(self):
+    return len(self.people)
 
 class Person:
     def __init__(self, age, income, gender):
@@ -32,10 +52,12 @@ class Person:
       self.income = int(income)
       if income == -19999:
         self.income = None
-      self.gender = gender
+      self.gender = 'm' if gender == 1 else 'f'
       self.household = 0
     def getAge(self):
       return self.age
+    def getGender(self):
+      return self.gender
 
 class UrbanActor:
     def __init__(self, person):
@@ -69,19 +91,67 @@ def import_household_data():
 
 def generate(n):
   print("-- GENERATE POPULATION --")
+  print("Sampling a population of size " + str(n) + "...")
   data, weights = import_person_data()
   samples = random.choices(data, k=n, weights=weights)
-  people = map(lambda p : Person(p[1],p[3],p[2]), samples)
+  population = list(map(lambda p : Person(p[1],p[3],p[2]), samples))
 
+  print("Fetching household data...")
   sample_households, hh_weights = import_household_data()
+  households = []
 
-  # we take the list of people and select households
-  hh = random.choices(sample_households, k=1, weights=hh_weights)[0]
-  hh_age = int(hh[1])
-  print(hh_age)
-  head_hh = list(filter(lambda p: p.getAge() == hh_age, people))
-  print(head_hh)
+  # get adult population information
+  adult_sample_households = list(filter(lambda hh : int(hh[1]) >= 18, sample_households))
+  selectors = [int(hh[1]) >= 18 for hh in sample_households]
+  adult_hh_weights = list(itertools.compress(hh_weights, selectors))
+    
+  rv = norm(scale=3) # normal distribution
+  rv_weights = []
+  for i in range(0,100):
+    w = []
+    for j in range(0,100):
+      w.append(rv.pdf(i-j))
+    rv_weights.append(w)
 
+  print("Generating selection of households...")
+  while len(population) > 0:
+    # we take the list of people and select households
+    # first, grab a household that we will select upon (filtered by adult presence)
+    hh_sample = random.choices(adult_sample_households, k=1, weights=adult_hh_weights)[0]
+    hh = Household()
+
+    # generate each time, population is changing
+    adult_population = list(filter(lambda p : p.getAge() >= 18, population))
+    if len(adult_population) > 0:
+      # -- PICK HEAD OF HOUSEHOLD --
+      # head age of this household (with reference to the sample data)
+      preferred_head_age = int(hh_sample[1])
+      # get population weights and grab a sample that is closest to the selected age
+      pop_weights = [rv_weights[preferred_head_age][p.getAge()] for p in adult_population]
+      hh_head = random.choices(adult_population, k=1, weights=pop_weights)[0]
+      population.remove(hh_head)
+      hh.addHead(hh_head)
+
+      # -- SELECT THE REMAINDER OF THE HOUSEHOLD BY SIZE --
+      hht = hh_sample[2]
+      if hht != 4 and hht != 6:
+        if hht == 1:
+          # fetch spouse
+          spouse = random.choices(adult_population, k=1, weights=pop_weights)[0]
+          population.remove(spouse)
+          hh.addSpouse(spouse)
+        for i in range(int(hh_sample[2])-hh.getSize()):
+          p = random.choice(population)
+          population.remove(p)
+          hh.addPerson(p)
+      households.append(hh)
+    else:
+      # At this point, we've exhausted our adult population and are left with children.
+      # Now we randomly go through households and replace adults with children
+      print([p.getAge() for p in population])
+      
+      break
+  print("Generated " + str(len(households)) + " households.")
 
 
 
