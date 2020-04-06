62d62952f0bc494568a540322bae1f11311a1022
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-04-06 11:53:31 -0400

    stuff

--- population.py
+++ population.py
@@ -2,10 +2,13 @@ from util.webapi import get_json, parse_query
 import numpy as np
 import random
 
+region_url = "https://api.census.gov/data/2018/acs/acs1/pums?get=PWGTP,AGEP,SEX,PINCP&ucgid=7950000US3703001,7950000US3703002"
+us_url = "https://api.census.gov/data/2018/acs/acs1/pums?get=PWGTP,AGEP,SEX,PINCP"
+
 # must have fields:
 #   household income
 #   household population density (sq mile)
-#   
+
 class Household:
   def __init__(self):
     pass
@@ -14,6 +17,8 @@ class Person:
     def __init__(self, age, income, gender):
       self.age = age
       self.income = income
+      if income == -19999:
+        self.income = None
       self.gender = gender
       self.household = 0
 
@@ -22,9 +27,7 @@ class UrbanActor:
         self.demographic = {}
         self.demographic.update(person.__dict__)
 
-def generate(n):
-  print("-- GENERATE POPULATION --")
-
+def import_data():
   # PWGTP - Weight, 
   # POWPUMA - Place of Work by subcounty (hotspot)
   # AGEP  - age
@@ -34,16 +37,19 @@ def generate(n):
   # PINCP - personal income
   # PUMA  - (ignore) region of no less than 10,000 people
   # ST    - State
-  url = "https://api.census.gov/data/2018/acs/acs1/pums?get=PWGTP,AGEP,SEX,PINCP"
-  res = get_json(url, {})
+  res = get_json(region_url, {})
 
-  headers = np.array(res)[0]
+  headers = res[0]
   print(headers)
-  data = np.array(res)[1:]
-  print(data)
-  print(data[:, 0].astype(int).sum())
+  data = res[1:]
+  weights = np.array(data)[:, 0].astype(int)
+  return data, weights
 
-  # print("-- GENERATE POPULATION --")
-  # print("Creating population of size", n)
-  # people = sample(n)
+def generate(n):
+  print("-- GENERATE POPULATION --")
+  data, weights = import_data()
+  samples = random.choices(data, k=n, weights=weights)
+  print(np.array(samples))
+
+  people = map(lambda p : Person(p[1],p[3],p[2]), samples)
 
